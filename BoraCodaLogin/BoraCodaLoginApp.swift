c72cad1594b6f413cbebea920c85d9e0b26a4c08
Felipe Vieira <felipefernandes.contato09@gmail.com>
2023-11-12 18:34:47 -0300

    Initial Commit

---
+++ BoraCodaLogin/BoraCodaLoginApp.swift
@@ -0,0 +1,17 @@
+//
+//  BoraCodaLoginApp.swift
+//  BoraCodaLogin
+//
+//  Created by Felipe Vieira on 12/11/23.
+//
+
+import SwiftUI
+
+@main
+struct BoraCodaLoginApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
