90fddafd9822ae64c6afebd140c1f4ca846ba12b
Felipe Vieira <felipefernandes.contato09@gmail.com>
2023-11-15 09:47:52 -0300

    add components to FormView

--- BoraCodaLogin/FormView.swift
+++ BoraCodaLogin/FormView.swift
@@ -9,7 +9,12 @@ import SwiftUI
 
 struct FormView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        LazyVStack {
+                
+            FieldUserView()
+            FieldPassView()
+                .padding(.top, 20)
+        }
     }
 }
 
