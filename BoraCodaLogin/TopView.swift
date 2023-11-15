a0b3c6f1148101485b532e1dd9408c39b105ebb4
Felipe Vieira <felipefernandes.contato09@gmail.com>
2023-11-15 09:46:43 -0300

    create top view

--- BoraCodaLogin/TopView.swift
+++ BoraCodaLogin/TopView.swift
@@ -9,7 +9,26 @@ import SwiftUI
 
 struct TopView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        
+        VStack {
+            Image("logo")
+                .padding(.top, 20)
+            
+            Text("Acesse a plataforma")
+                .font(.custom("TitilliumWeb-Bold", size: 30))
+                .padding(.top, 30)
+                .padding(.bottom, 20)
+                .foregroundStyle(.gray800)
+            
+            Text("Faça login ou registre-se para começar a construir seus projetos ainda hoje.")
+                .font(.custom("TitilliumWeb-Light", size: 16))
+                .padding(.horizontal, 20)
+                .multilineTextAlignment(.center)
+                .foregroundStyle(.gray600)
+            
+            
+        }
+        
     }
 }
 
